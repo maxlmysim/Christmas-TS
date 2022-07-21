91f41b0413b7614e4a68e0df627c980313652eb3
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-07-21 12:36:29 +0300

    feat: deploy

--- src/ts/filtersMenu/slider/slider-years.ts
+++
@@ -1,9 +0,0 @@
-import {SliderCreator} from "./sliderCreator";
-import {paramsToys, sliderParams} from "../../settings";
-
-export class SliderYears extends SliderCreator {
-    public constructor() {
-        super(sliderParams.years.name, paramsToys.minYear, paramsToys.maxYear, +sliderParams.years.step, sliderParams.years.text);
-    }
-}
-
