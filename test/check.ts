fff326c3a40cbea75013882e395b77696fc41faa
Maksim Lapko <94135700+maxlmysim@users.noreply.github.com>
2022-07-27 19:14:12 +0300

    feat: add tests

--- test/check.ts
+++ test/check.ts
@@ -33,6 +33,136 @@ describe('Settings', function () {
         });
     })
 
+    describe('filtersSettings', function () {
+        const data = JSON.parse('[{"num":"1","name":"Большой шар с рисунком","count":"2","year":"1960","shape":"шар","color":"желтый","size":"большой","favorite":false},{"num":"2","name":"Зелёный шар с цветами","count":"5","year":"2000","shape":"шар","color":"зелёный","size":"большой","favorite":false},{"num":"3","name":"Красный матовый шар","count":"3","year":"1990","shape":"шар","color":"красный","size":"большой","favorite":false},{"num":"4","name":"Сосулька красная","count":"2","year":"1980","shape":"фигурка","color":"красный","size":"большой","favorite":false},{"num":"5","name":"Красный виноград","count":"4","year":"1980","shape":"фигурка","color":"красный","size":"средний","favorite":true}]')
+        it('year selection', function () {
+            filtersSettings.yearMin = 1950
+            filtersSettings.yearMax = 1960
+            const expected = [
+                {
+                    color: 'желтый',
+                    count: '2',
+                    favorite: false,
+                    name: 'Большой шар с рисунком',
+                    num: '1',
+                    shape: 'шар',
+                    size: 'большой',
+                    year: '1960'
+                }
+            ]
+
+            assert.deepEqual(yearSelection(data), expected);
+
+        });
+        it('shape selection', function () {
+            filtersSettings.shape = ['шар']
+            const expected = [
+                {
+                    color: 'желтый',
+                    count: '2',
+                    favorite: false,
+                    name: 'Большой шар с рисунком',
+                    num: '1',
+                    shape: 'шар',
+                    size: 'большой',
+                    year: '1960'
+                },
+                {
+                    color: 'зелёный',
+                    count: '5',
+                    favorite: false,
+                    name: 'Зелёный шар с цветами',
+                    num: '2',
+                    shape: 'шар',
+                    size: 'большой',
+                    year: '2000'
+                },
+                {
+                    num: "3",
+                    name: "Красный матовый шар",
+                    count: "3",
+                    year: "1990",
+                    shape: "шар",
+                    color: "красный",
+                    size: "большой",
+                    favorite: false
+                }
+            ]
+
+            assert.deepEqual(shapeSelection(data), expected);
+        });
+        it('color selection', function () {
+            filtersSettings.color = ['зелёный']
+            const expected = [{
+                color: 'зелёный',
+                count: '5',
+                favorite: false,
+                name: 'Зелёный шар с цветами',
+                num: '2',
+                shape: 'шар',
+                size: 'большой',
+                year: '2000'
+            }]
+
+            assert.deepEqual(colorSelection(data), expected);
+        });
+        it('search selection', function () {
+            filtersSettings.search = 'шар с цветами'
+            const expected = [{
+                color: 'зелёный',
+                count: '5',
+                favorite: false,
+                name: 'Зелёный шар с цветами',
+                num: '2',
+                shape: 'шар',
+                size: 'большой',
+                year: '2000'
+            }]
+            assert.deepEqual(colorSelection(data), expected);
+        });
+        it('popular selection', function () {
+            const expected = [{
+                num: "5",
+                name: "Красный виноград",
+                count: "4",
+                year: "1980",
+                shape: "фигурка",
+                color: "красный",
+                size: "средний",
+                favorite: true
+            }]
+            assert.deepEqual(popularSelection(data), expected);
+        });
+        it('size selection', function () {
+            filtersSettings.size = ['средний']
+            const expected = [{
+                num: "5",
+                name: "Красный виноград",
+                count: "4",
+                year: "1980",
+                shape: "фигурка",
+                color: "красный",
+                size: "средний",
+                favorite: true
+            }]
+            assert.deepEqual(sizeSelection(data), expected);
+        });
+        it('quantity selection', function () {
+            filtersSettings.quantityMin = 5
+            filtersSettings.quantityMax = 5
+            const expected = [{
+                num: "5",
+                name: "Красный виноград",
+                count: "4",
+                year: "1980",
+                shape: "фигурка",
+                color: "красный",
+                size: "средний",
+                favorite: true
+            }]
+            assert.deepEqual(sizeSelection(data), expected);
+        });
+    });
 })
 
 
